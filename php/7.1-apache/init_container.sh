755ac0c95e52c2ac1af719d1235b56bb6a1a42bc
sunitha muthukrishna <sumuth@microsoft.com>
2018-02-01 15:18:03 -0800

    added comment in init.sh

--- php/7.1-apache/init_container.sh
+++ php/7.1-apache/init_container.sh
@@ -17,6 +17,7 @@ cat /etc/motd
 # Get environment variables to show up in SSH session
 eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
 
+#Starting SSH 
 service ssh start
 sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
 mkdir /var/lock/apache2

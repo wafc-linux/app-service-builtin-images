8c3ed672382c61c56a39455574659c4142ce25f4
Web App for Containers <35706733+wafc-linux@users.noreply.github.com>
2018-02-02 13:05:06 -0800

    Update main.sh

--- travis-script/main.sh
+++ travis-script/main.sh
@@ -199,15 +199,16 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo $TRAVIS_COMMIT
     echo $TRAVIS_COMMIT >> result.log
     SignOff="#sign-off"        
-    sign0ff=$(echo "${TRAVIS_COMMIT_MESSAGE}" | grep "${SignOff}")
-    if [ -n "${signoff}" ]; then
+    signOff=$(echo "${TRAVIS_COMMIT_MESSAGE}" | grep "${SignOff}")
+    if [ -n "${signOff}" ]; then
         echo "========================================"
         echo "========================================" >> result.log
         echo "INFORMATION - COMMIT MESSAGE contains #sign-off, using PROD......"
         echo "INFORMATION - COMMIT MESSAGE contains #sign-off, using PROD......" >> result.log
-        ACR_USERNAME=$PROD_ACR_USERNAME
-        ACR_PASSWORD=$PROD_ACR_PASSWORD
-	    ACR_SERVER = $PROD_ACR_SERVER
+        DOCKER_USERNAME=$PROD_DOCKER_USERNAME
+        DOCKER_ACCOUNT=$PROD_DOCKER_ACCOUNT
+        DOCKER_PASSWORD=$PROD_DOCKER_PASSWORD
+	ACR_REPO= $PROD_DOCKER_ACCOUNT
     fi    
     commit_sha=$TRAVIS_COMMIT    
     get_files_from_commit
